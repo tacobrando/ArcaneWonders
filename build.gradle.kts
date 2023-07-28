9b82d5fe404918ee3fd7ac70247136f644b9131c
Matthew Lee <50124913+tacobrando@users.noreply.github.com>
2023-07-28 04:31:51 -0400

    converted gradle build from Groovy to Kotlin && Fixed duplicate recipe issue

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    `maven-publish`
+    kotlin("jvm") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("me.tagavari.nmsremap") version "1.0.0"
+    `distribution`
+}
+
+group = "dev.tacobrando"
+version = "1.0-ALPHA"
+
+repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://maven.enginehub.org/repo/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot:1.20.1-R0.1-SNAPSHOT:remapped-mojang")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+        }
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.named<Copy>("processResources") {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("shadow")
+    // export final jar to server plugins folder
+    doLast {
+        val exportPath = project.findProperty("plugin.export")
+
+        if (exportPath is String) {
+            copy {
+                from(archiveFile.get())
+                into(File(exportPath))
+                rename(".*", "ArcaneWonders.jar")
+            }
+            println("Exported plugin to $exportPath")
+        } else {
+            println("Provide plugin export path in 'gradle.properties'")
+        }
+    }
+}
+
+tasks.named<Jar>("jar") {
+    dependsOn("shadowJar")
+}
+
+publishing {
+    publications {
+        register("maven", MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+}
--- src/main/kotlin/dev/tacobrando/arcanewonders/recipes/WandRecipe.kt
+++ src/main/kotlin/dev/tacobrando/arcanewonders/recipes/WandRecipe.kt
@@ -2,21 +2,26 @@ package dev.tacobrando.arcanewonders.recipes
 
 import dev.tacobrando.arcanewonders.ArcaneWonders
 import org.bukkit.Bukkit
-import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.inventory.ItemStack
+import org.bukkit.Material
 import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+
 
 object WandRecipe {
     fun register(item: ItemStack, key: String) {
         val verticalKey = NamespacedKey(ArcaneWonders.instance, key)
-        val verticalRecipe = ShapedRecipe(verticalKey, item)
 
-        verticalRecipe.shape("E", "B", "B")
-        verticalRecipe.setIngredient('B', Material.BLAZE_ROD)
-        verticalRecipe.setIngredient('E', Material.ENDER_PEARL)
-
-        Bukkit.addRecipe(verticalRecipe)
+        if(Bukkit.getRecipe(verticalKey) == null) {
+            val verticalRecipe = ShapedRecipe(verticalKey, item)
+        
+            verticalRecipe.shape("E", "B", "B")
+            verticalRecipe.setIngredient('B', Material.BLAZE_ROD)
+            verticalRecipe.setIngredient('E', Material.ENDER_PEARL)
+    
+            
+            Bukkit.addRecipe(verticalRecipe)
+        }
     }
 }
